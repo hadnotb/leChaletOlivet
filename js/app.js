ecb5085e438433a7cbcf4c2215295c6eda7ae781
Hichem <adnot.hichem@gmail.com>
2023-05-22 07:38:41 +0200

    up

--- js/app.js
+++ js/app.js
@@ -1,7 +1,46 @@
-var image = document.querySelectorAll('img');
-console.log(image);
-new simpleParallax(image,{
-	speed: 1.5, // vitesse de défilement plus rapide
-    intensity: 70, // intensité de l'effet de parallaxe plus élevée
-    perspective: 500
+// var image = document.querySelectorAll('img');
+// console.log(image);
+// new simpleParallax(image,{
+// 	speed: 1.5, // vitesse de défilement plus rapide
+//     intensity: 70, // intensité de l'effet de parallaxe plus élevée
+//     perspective: 500
+// });
+
+
+
+
+
+
+let defaultCursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", cursor);
+window.addEventListener('click', scale);
+
+function scale(e) {
+  defaultCursor.classList.toggle('click-grow');
+}
+
+function cursor(e) {
+  defaultCursor.style.top = e.pageY + "px";
+  defaultCursor.style.left = e.pageX + "px";
+}
+
+document.querySelector('.siteHeader-mail').addEventListener("click", function() {
+  let overlay = document.querySelector("#overlay");
+  let embedSU = document.querySelector("#mc_embed_signup");
+  console.log(overlay,embedSU);
+  overlay.style.display = "flex";
+  embedSU.style.display = "block";
+ 
 });
+
+document.getElementById("closeBtn").addEventListener("click", function() {
+  document.getElementById("overlay").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == document.getElementById("overlay")) {
+      document.getElementById("overlay").style.display = "none";
+  }
+});
+
