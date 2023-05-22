8355b11e77688585a06c1e84534490a53b8f3ba9
Hichem <hadnot@initiative-sud.com>
2023-05-22 13:02:14 +0200

    upd

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 var image = document.querySelectorAll('img');
 console.log(image);
 new simpleParallax(image,{
-	speed: 1.5, // vitesse de défilement plus rapide
-    intensity: 70, // intensité de l'effet de parallaxe plus élevée
-    perspective: 500
+	delay: .6,
+	transition: 'cubic-bezier(0,0,0,1)'
 });
