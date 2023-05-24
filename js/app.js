9263b88a72847a77e177fd8fc4c050b6e52868c9
Hichem <hadnot@initiative-sud.com>
2023-05-24 13:13:47 +0200

    up

--- js/app.js
+++ js/app.js
@@ -27,20 +27,31 @@
 let overlay = document.querySelector("#overlay");
 let embedSU = document.querySelector("#mc_embed_signup");
 let form = document.querySelector("#mc-embedded-subscribe-form");
+let input = document.querySelector('#mce-EMAIL');
+
 
 document.querySelector('.news').addEventListener("click", function() {
   overlay.style.display = "flex";
   embedSU.style.display = "block";
- 
 });
 
 document.getElementById("closeBtn").addEventListener("click", function() {
   document.getElementById("overlay").style.display = "none";
+  input.style="border: 1px solid #ABB0B2;";
+  var divEnfant = document.querySelector('.mc-field-group div');
+  if(divEnfant){
+    divEnfant.style.display="none";
+  }
 });
 
 window.addEventListener("click", function(event) {
   if (event.target == document.getElementById("overlay")) {
       document.getElementById("overlay").style.display = "none";
+      input.style="border: 1px solid #ABB0B2;";
+      var divEnfant = document.querySelector('.mc-field-group div');
+      if(divEnfant){
+        divEnfant.style.display="none";
+      }
   }
 });
 
